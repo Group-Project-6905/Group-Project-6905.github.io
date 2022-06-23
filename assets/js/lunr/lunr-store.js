b94cb38ce143dfba3220d869f7d9a82896b8113d
Henry Yip <betelgeuse0125@gmail.com>
2022-06-24 00:14:28 +0800

    23/06/2022

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Sample Post (Please Check!)",
+        "excerpt":"Note: This is just a placeholder to showcase how the post will look like. I’ll delete these after some time. How to include Math Symbols in Jekyll Note that in this website, we use \\(\\KaTeX\\) instead of \\(\\LaTeX\\). Example: \\(T=\\frac{4(\\frac{E}{U_0})(1-\\frac{E}{U_0})}{\\sinh^{2}{\\sqrt{2m(U_0-E)}\\frac{L}{\\hbar}}+4(\\frac{E}{U_0}){(1-\\frac{E}{U_0})}}\\) If you want to use \\(\\KaTeX\\) in Jekyll, you first...","categories": [],
+        "tags": [],
+        "url": "/Katex/",
+        "teaser": null
+      }]
