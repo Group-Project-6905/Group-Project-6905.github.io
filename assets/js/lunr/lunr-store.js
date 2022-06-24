75a20a40e7245c256d7f0770456f6f615afa552f
Henry Yip <betelgeuse0125@gmail.com>
2022-06-24 15:44:27 +0800

    Google Analytics

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,4 +1,22 @@
 var store = [{
+        "title": "Allison",
+        "excerpt":"Hello my name is Allison.                                                    This is a sample picture       Bye.  ","categories": [],
+        "tags": [],
+        "url": "/Founders/Allison/",
+        "teaser": "/assets/images/Central.jpg"
+      },{
+        "title": "Esther",
+        "excerpt":"Hello my name is Esther.                                                    This is a sample picture       Bye.  ","categories": [],
+        "tags": [],
+        "url": "/Founders/Esther/",
+        "teaser": "/assets/images/Central.jpg"
+      },{
+        "title": "Henry",
+        "excerpt":"Hello my name is Henry.                                                    This is a sample picture       Bye.  ","categories": [],
+        "tags": [],
+        "url": "/Founders/Henry/",
+        "teaser": "/assets/images/Central.jpg"
+      },{
         "title": "Sample Post (Please Check!)",
         "excerpt":"Note: This is just a placeholder to showcase how the post will look like. I’ll delete these after some time. How to include Math Symbols in Jekyll Note that in this website, we use \\(\\KaTeX\\) instead of \\(\\LaTeX\\). Example: \\(T=\\frac{4(\\frac{E}{U_0})(1-\\frac{E}{U_0})}{\\sinh^{2}{\\sqrt{2m(U_0-E)}\\frac{L}{\\hbar}}+4(\\frac{E}{U_0}){(1-\\frac{E}{U_0})}}\\) If you want to use \\(\\KaTeX\\) in Jekyll, you first...","categories": [],
         "tags": [],
